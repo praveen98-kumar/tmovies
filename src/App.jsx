494eda32aac3e35aef3916009719ad6fc5c377cd
Praveen Kumar <pkkoram98@gmail.com>
2022-03-25 19:51:54 +0530

    fix import

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import 'swiper/css/scrollbar';
 import './App.scss'
 import { BrowserRouter } from 'react-router-dom'
 
-import Header from './components/header/header'
+import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 
 import Routes from './config/Routes'
